bf22e0bfbf444ca3e1bf719e91742ff8350c6067
Ottaviorr <otavioherdy2005@gmail.com>
2024-06-19 09:09:51 -0300

    V1.0 - Teste_DecToBin_BinToDec

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(' ')
+  const [decimal, setDecimal] = useState(' ')
+
+  function convertToBin(n:any) {
+    return (n >>> 0).toString(2);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Binario para decimal</h1>
+      <label>Digite um numero: </label>
+      <input type="text" onChange={(e)=>setCount(e.target.value)}/>
+      <p>{parseInt(count, 2)}</p>
+
+      <h1>Decimal para Binario</h1>
+      <label>Digite um numero: </label>
+      <input type="number" onChange={(e)=>setDecimal(e.target.value)}/>
+      <p>{convertToBin(decimal)}</p>
     </>
   )
 }
