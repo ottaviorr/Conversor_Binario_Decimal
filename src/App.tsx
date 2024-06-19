594230c37dfae3c9af635200cc96fff1b247d11d
Ottaviorr <otavioherdy2005@gmail.com>
2024-06-19 19:19:28 -0300

    Projeto Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,27 @@ function App() {
     return (n >>> 0).toString(2);
   }
 
+  function handleBinaryChange(e:any){
+    const value = e.target.value
+    setCount(value);
+  }
+
+  
+  function handleDecimalChange(e:any){
+    const value = e.target.value
+    setDecimal(value);
+  }
+
   return (
     <>
       <h1>Binario para decimal</h1>
       <label>Digite um numero: </label>
-      <input type="text" onChange={(e)=>setCount(e.target.value)}/>
+      <input type="text" onChange={handleBinaryChange} value={count}/>
       <p>{parseInt(count, 2)}</p>
 
       <h1>Decimal para Binario</h1>
       <label>Digite um numero: </label>
-      <input type="number" onChange={(e)=>setDecimal(e.target.value)}/>
+      <input type="text" onChange={handleDecimalChange} value={decimal}/>
       <p>{convertToBin(decimal)}</p>
     </>
   )
